0510f484649b7aa4958a3495d4629d0953580db8
jinyul80 <jinyul80@gmail.com>
2021-12-28 22:50:31 +0900

    v 0.0.3
    React-route  적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Post from './pages/Post';
+import User from './pages/User';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/post" component={Post}></Route>
+        <Route path="/user" component={User}></Route>
+      </Switch>
     </div>
   );
 }
